8be0ad96cb1ba33c767caf1f1d7fa5d834bf5a5e
brenohq <brenosc2@hotmail.com>
2021-02-22 09:41:27 -0300

    creating route to sum numbers for a given filename

--- lib/rocketpay_web/controllers/welcome_controller.ex
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -1,7 +1,25 @@
 defmodule RocketpayWeb.WelcomeController do
   use RocketpayWeb, :controller
 
-  def index(conn, _params) do
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+
     text(conn, "Welcome to the Rocketpay API")
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to the Rocketpay API. Here is your sum: #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
